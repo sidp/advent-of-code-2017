12f74c81fc4e27fdaea5374da217332c1d8ca285
Peter Simonsson <peter@simonsson.com>
2017-12-07 22:39:20 +0100

    December 5

---
+++ src/5-b.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const input = fs.readFileSync(path.join(__dirname, '../inputs/5.txt'), 'utf-8');
+
+const list = input.split('\n').map(item => parseInt(item, 10));
+
+let current = 0;
+let steps = 0;
+
+while (list[current] !== undefined) {
+	let jumpOffset = list[current];
+	if (jumpOffset >= 3) {
+		list[current] -= 1;
+	} else {
+		list[current] += 1;
+	}
+	current += jumpOffset;
+	steps += 1;
+}
+
+module.exports = steps;
